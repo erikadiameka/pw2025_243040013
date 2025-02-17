290db5b1f45d723239aea52f12efe0eb7b37d776
erikadiameka <erikardiameka@gmail.com>
2025-02-17 07:17:50 +0700

    tugas1

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,12 @@
+<?php
+    $nama = "Erik aja";
+    $email = "erikardiameka@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Informatika";
+    $universitas = "Universitas Pasundan";
+
+   echo "<h1>$nama</h1>";
+   echo "<h4>$email</h4>";
+   echo  "<p>Hallo, Nama Saya $nama, Saya seorang $pekerjaan Teknik $jurusan $universitas</p>";
+
+?>
